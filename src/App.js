ecb17920d07cd3ae198f5a4608376f66e48c45ed
spookiebruh <zackeboibeat@gmail.com>
2022-11-29 11:02:16 +0100

    First commit from Zacke (no real stuff)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import { Route, Routes } from "react-router-dom";
-
-import login from "./page/login";
-
+import BG from './bilder/andrew-neel-cckf4TsHAuw-unsplash.jpg';
+import './App.css';
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Back from "./component/Back";
+import { Link } from "react-router-dom";
 
 function App() {
+  const [isOpen, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!isOpen);
+  };
+
   return (
-    <div>
+    <div className="con">
+      <button type="button" onClick={handleClick} id="back" class="arrow">Back</button>
+      <button type="button" onClick={handleClick} id="forward" class="arrow">Forward</button>
+      <img src={BG} alt="background image"/>
+      <div className="window blur">
+        <Link to="/Home">Kalender</Link>
+        <Link to="/Back">tillbaka</Link>
+        <button type="button" onClick={handleClick}>Suspicious button</button>
+        
+      </div>
       <Routes>
-        <Route path="/" element ={<login />} />      
+        <Route path="/Home" element={ <Home/> } />
+        <Route path="/Back" element={ <Back/> } />
       </Routes>
+      
     </div>
   );
 }
+
 
 export default App;
