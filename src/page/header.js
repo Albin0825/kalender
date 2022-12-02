8b989af691c53fd9df6cab4eb42bd5b60c787bc9
Albin0825 <albinkarlsson2003@gmail.com>
2022-12-02 14:14:28 +0100

    you can login :)

--- src/page/header.js
+++ src/page/header.js
@@ -1,27 +1,36 @@
 import { Link } from "react-router-dom";
+import '../App.css';
 
 
 function HeaderBar() {
     let setup =
-        <nav style={{
+        <nav>
+            <ul style={{display: "flex", justifyContent: "flex-end"}}>
+                <li style={{display: "inline-block", textAlign: "center"}}>
+                    <Link style={{textDecoration: "none"}} to="/Home">Kalender</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
+                    <Link style={{textDecoration: "none"}} to='/Login'>Login</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
+                    <Link style={{textDecoration: "none"}} to='/OneEventOpen'>OneEventOpen</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
+                    <Link style={{textDecoration: "none"}} to="/Hamburger">Hamburger meny</Link>
+                </li>
+            </ul>
+        </nav>;
+    return (
+        <header style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
             background: "linear-gradient(135deg, rgba(255,255,255,0.75) 0%, rgba(255,255,255,0.12) 100%)",
             width: "100%",
             height: "6vh",
             backdropFilter: "blur(40px)"
             }}>
-            <ul>
-                <li>
-                    <Link style={{textDecoration: "none"}} to="/Home">Kalender</Link>
-                    <Link style={{textDecoration: "none"}} to="/Back">tillbaka</Link>
-                    <Link style={{textDecoration: "none"}} to='/Login'>Login</Link>
-                    <Link style={{textDecoration: "none"}} to='/OneEventOpen'>OneEventOpen</Link>
-                    <Link style={{textDecoration: "none"}} to="/Hamburger">Hamburger meny</Link>
-                </li>
-            </ul>
-        </nav>;
-    console.log(setup)
-    return (
-        <header>
+            <img src="" alt="ACE SOFT"></img>
             {setup}
         </header>
     );
