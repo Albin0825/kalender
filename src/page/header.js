3047d8a570da049042362a6aa0ac940015d132f6
Rasmus Florin <rasmus.florin@elev.ga.ntig.com>
2022-12-05 09:05:02 +0100

    header conflict resolved

--- src/page/header.js
+++ src/page/header.js
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
+import '../App.css';
 
 
 function HeaderBar() {
     let setup =
-        <nav style={{
-            background: "linear-gradient(135deg, rgba(255,255,255,0.75) 0%, rgba(255,255,255,0.12) 100%)",
-            width: "100%",
-            height: "6vh",
-            backdropFilter: "blur(40px)"
-            }}>
-            <ul>
-                <li>
+        <nav>
+            <ul style={{display: "flex", justifyContent: "flex-end"}}>
+                <li style={{display: "inline-block", textAlign: "center"}}>
                     <Link style={{textDecoration: "none"}} to="/Home">Kalender</Link>
-                    <Link style={{textDecoration: "none"}} to="/Back">tillbaka</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
                     <Link style={{textDecoration: "none"}} to='/Login'>Login</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
                     <Link style={{textDecoration: "none"}} to='/OneEventOpen'>OneEventOpen</Link>
+                </li>
+                <li style={{display: "inline-block", textAlign: "center"}}>
                     <Link style={{textDecoration: "none"}} to="/Hamburger">Hamburger meny</Link>
                 </li>
             </ul>
         </nav>;
     return (
-        <header>
+        <header style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            background: "linear-gradient(135deg, rgba(255,255,255,0.75) 0%, rgba(255,255,255,0.12) 100%)",
+            width: "100%",
+            height: "6vh",
+            backdropFilter: "blur(40px)"
+            }}>
+            <img src="" alt="ACE SOFT"></img>
             {setup}
         </header>
     );
