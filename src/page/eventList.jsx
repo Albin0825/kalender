ef8112fd6e833fb33c1c4a02f49fa0b9a7ad53fd
spookiebruh <zackeboibeat@gmail.com>
2022-12-07 09:42:36 +0100

    VI HAR COOKIES!!!

--- src/page/eventList.jsx
+++ src/page/eventList.jsx
@@ -1,20 +1,23 @@
 import '../App.css';
 import { useEffect, useState } from "react";
-import * as React from 'react';
+import React from 'react';
 import Event from '../component/Event';
 import { loadLs } from '../component/Funktioner';
 
 function Eventlista(response){
     const [uid, setUid] = useState(loadLs('uID'));
     const [token, setToken] = useState(loadLs('token'));
-    const API_URL = "https://takeee.ntigskovde.se/Calendar/calendar_index.php?action=showEvent&uID="+uid+"&token"+token;
     const [eventlist, setEventlist] = useState([]);
+
+    
     const getEvents = async () => {
-        //let API_URL = "https://takeee.ntigskovde.se/Calendar/calendar_index.php?action=showEvent&uID="+userid+"&token="+token;
+        let API_URL = "https://takeee.ntigskovde.se/Calendar/calendar_index.php?action=showEvent&uID="+uid+"&token="+token;
         const response = await fetch(`${API_URL}`)
         .then(response => response.json())
         .then(
             (result)=>{
+                console.log(loadLs());
+                console.log("bruh", loadLs('token'));
                 console.log(result["Data"]["My events"]);
                 setEventlist(result["Data"]["My events"]);
             }
