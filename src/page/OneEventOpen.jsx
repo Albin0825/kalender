750fbf437c8d7dae8dcf08d36940101c3e85a35e
Albin0825 <albinkarlsson2003@gmail.com>
2023-01-09 14:08:39 +0100

    hamber code is only in one file now, days work :)

--- src/page/OneEventOpen.jsx
+++ src/page/OneEventOpen.jsx
@@ -1,31 +1,22 @@
+import React, {useState, useEffect} from "react"
+import { Link } from 'react-router-dom';
+import Hamburger from '../component/Hamburger'
+import '../App.css';
 import BG from '../bilder/marita-kavelashvili-ugnrXk1129g-unsplash.jpg';
 import ARROW from '../bilder/Vector.svg';
-import React, {useState, useEffect} from "react"
-import '../App.css';
-import { Link } from 'react-router-dom';
 
 function App() {
-
-  const [navbarOpen, setNavbarOpen] = useState(false)
-    const handleToggle = () => {
-        setNavbarOpen(prev => !prev)
-    }
-
   return (
     <div className="con">
       <img src={BG} alt="background image"/>
-        <div className="window blur notlogin">
-          <Link to='/eventList' className="vpil"><img src={ARROW} alt="Go back"/></Link>
-          <h1 className="rubrik">Rubrik!</h1>
-          <p className="desc">Beskrivning: </p>
-          <p className="time">Tid: </p>
-          <p className="inv">Inbjudningar: </p>
-          <nav className="navBar">
-              <button className={`button ${navbarOpen ? " showMenu" : "noMenu"}`} onClick={handleToggle}></button>
-              <Link className={`button menuNav2 ${navbarOpen ? " showMenu" : "noMenu"}`} to="/Editevent" />
-              <Link className={`button menuNav3 ${navbarOpen ? " showMenu" : "noMenu"}`} to="/Delete" />
-          </nav>
-        </div>
+      <div className="window blur notlogin">
+        <Link to='/eventList' className="vpil"><img src={ARROW} alt="Go back"/></Link>
+        <h1 className="rubrik">Rubrik!</h1>
+        <p className="desc">Beskrivning: </p>
+        <p className="time">Tid: </p>
+        <p className="inv">Inbjudningar: </p>
+        <Hamburger/>
+      </div>
     </div>
   );
 }
--- src/page/allEventList.jsx
+++ src/page/allEventList.jsx
@@ -1,46 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { loadLs } from '../component/Funktioner';
+import Hamburger from '../component/Hamburger'
 import '../App.css';
 import BG from '../bilder/marita-kavelashvili-ugnrXk1129g-unsplash.jpg';
 import ARROW from '../bilder/Vector.svg';
-import { useEffect, useState } from "react";
-import React from 'react';
-import Event from '../component/Event';
-import { loadLs } from '../component/Funktioner';
-import { Link } from 'react-router-dom';
 
-function Alleventlist(response){
-
-    const [navbarOpen, setNavbarOpen] = useState(false)
-    const handleToggle = () => {
-        setNavbarOpen(prev => !prev)
-    }
-
+function Alleventlist(){
     const [uid] = useState(loadLs('uID'));
     const [token] = useState(loadLs('token'));
-    const [eventlist, setEventlist] = useState([]);
-    
-
-    
-    const getEvents = async () => {
-        let API_URL = "https://takeee.ntigskovde.se/Calendar/calendar_index.php?action=showEvent&uID="+uid+"&token="+token;
-        const response = await fetch(`${API_URL}`)
-        .then(response => response.json())
-        .then(
-            (result)=>{
-                console.log(loadLs());
-                console.log(loadLs('token'));
-                console.log(result);
-                console.log(result["Data"]["My events"]);
-                setEventlist(result["Data"]["My events"]); 
-                
-            }
-        )
-        
-       //const data = await response.json(); 
-    }
-    
-    useEffect(() => {
-        getEvents();
-      }, []);
 
     return(
         <div className='con'>
@@ -48,16 +16,7 @@ function Alleventlist(response){
                 <div className='window blur eventList'>
                     <Link to='/Kalender' className="vpil"><img src={ARROW} alt="Go back"/></Link>
                     <p className="OneEventOpenRubrik">Dina händelser</p>
-                    {
-                    eventlist.map((events) => (
-                    <Event key={events["ID"].toString()} event={events} />    
-                    ))}
-                <nav className="navBar">
-                    <button className={`button ${navbarOpen ? " showMenu" : "noMenu"}`} onClick={handleToggle}></button>
-                    <Link className={`button menuNav1 ${navbarOpen ? " showMenu" : "noMenu"}`} to="/add"/>
-                    <Link className={`button menuNav2 ${navbarOpen ? " showMenu" : "noMenu"}`} to="/editevent"/>
-                    <Link className={`button menuNav3 ${navbarOpen ? " showMenu" : "noMenu"}`} to="/delete" />
-                </nav>
+                    <Hamburger/>
                 </div>
         </div>
     )
